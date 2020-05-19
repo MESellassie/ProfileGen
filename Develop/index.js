d9ca43e3fec53495587b441af330d82d02d01449
Micah Evans <micah.s.evans@gmail.com>
2020-05-18 21:43:25 -0400

    Building our index.js functionality

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,37 @@
-const questions = [
-  
-];
-
-function writeToFile(fileName, data) {
+// function writeToFile(fileName, data) {
  
-}
+// }
+
+// function init() {
+
+// init();
+
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require ("inquirer");
+const convertFactory = require(`electron-html-to`);
+
+const conversion = convertFactory({ converterPath: convertFactory.converters.PDF });
+
+let data = {};
+
+let questions = [
+    {
+        message: `What is your github username?`,
+        name: `username`,
+    },
+    {
+        message: `What is your favorite color`,
+        name: `color`,
+        type: `list`,
+        choices: [`blue`, `red`, `black`, `green`]
+    }
+]
 
 function init() {
-
-init();
+    inquirer
+    .prompt(questions)
+    .then(function ({username, color}) {
+        const queryUrl = `https://api.github.com/users/${username}`;
+    })
+}
